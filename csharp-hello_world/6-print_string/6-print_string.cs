474d62af1aaf171f68f8470a3695d06a0077bef4
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-17 19:25:25 +0100

    differentApproach

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,8 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		string str2 = $"{str}{str}{str}";
-		Console.WriteLine(str2);
+		Console.WriteLine($"{str}{str}{str}");
 		Console.WriteLine($"{str.SubString(0,9)}");
         }
 }
