fc5ba46c1521e4bc56f14e5114217410de67949b
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-17 19:16:55 +0100

    forgot the dot

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str = "Holberton School";
 		string str2 = $"{str}{str}{str}";
-		ConsoleWriteLine(str2);
-		ConsoleWriteLine(str.SubString(0,9));
+		Console.WriteLine(str2);
+		Console.WriteLine(str.SubString(0,9));
         }
 }
