be58769b3a193770f278485ead6670824e90d472
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 22:56:12 +0100

    test

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+	int character = (int)c;
+	if(character >= 97 && character <= 122) {
+		return true;
+		}
+	else {
+		return false;
+		}
+    }
+    }
+}
