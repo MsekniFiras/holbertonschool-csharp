b09b7d2d473d7624fe3046581f5d2ece7640ba17
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 23:45:59 +0100

    test

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		for (int number = 1;number <= 99; number++) { 
+		if (number % 3 == 0) {
+		Console.Write("Fizz ");
+				}
+		else if (number % 5 == 0) {
+		Console.Write("Buzz ");
+				}
+		else if (number % 3 == 0 & number % 5 == 0) {
+			Console.Write("FizzBuzz");
+				}
+		else {
+            Console.Write($"{number} ");
+		}
+						}        
+		Console.WriteLine("Buzz");
+	}
+	
+    }
+}
