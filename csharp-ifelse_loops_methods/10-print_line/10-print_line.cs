649b7611fd0f16575c24309a8f8886919c8abb16
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 23:20:48 +0100

    test

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+	if (length <= 0) {
+            Console.WriteLine();
+			}
+	else {
+		for (int i = 0; i < length; i++) {
+			Console.Write("_");
+					}
+		Console.Writeline();
+	}
+
+	}
+    }
+}
