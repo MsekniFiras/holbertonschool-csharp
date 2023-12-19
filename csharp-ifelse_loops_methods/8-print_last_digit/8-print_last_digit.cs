c1387fe13bf44b4909e9939b8b81b6c1f4bb2720
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 23:02:59 +0100

    test

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Math;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+		int lastdigit = Abs.(number) % 10;
+            Console.Write(lastdigit);
+        }
+    }
+}
