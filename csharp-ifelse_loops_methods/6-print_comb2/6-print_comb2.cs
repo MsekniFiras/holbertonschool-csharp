e79022ee8f29073355e79951fd5da95bc4ce1ae6
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 19:49:15 +0100

    braces

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -9,12 +9,12 @@ namespace _6_print_comb2
 	for (int i = 0; i <=8; i++) {
 		for (int j = 0; j <= 9; j++) {
 			if (i < j) {
-				if ((i < 8 || j < 9) {
+				if ((i < 8 || j < 9) 
 					Console.Write($"{i}{j}, ");
-				}
-				else {
+				
+				else 
 				Console.Write($"{i}{j}");
-				}
+				
 				}
             Console.WriteLine();
         }
