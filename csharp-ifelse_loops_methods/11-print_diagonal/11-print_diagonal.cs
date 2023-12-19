0a34a8a14de9733aa4205972f6e7984a7a37cf58
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 23:31:48 +0100

    test

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+	if (length <= 0) {
+            Console.WriteLine();
+		}
+	else {
+	for (int i = 0; i <= length; i++) {
+		Console.Write("\\");
+			}
+        }
+	Console.WriteLine();
+    }
+    }
+}
