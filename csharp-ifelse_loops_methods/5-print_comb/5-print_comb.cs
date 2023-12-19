7b60158fa1b702478f86ad5c67ca63d227c5ef08
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2023-12-19 15:18:40 +0100

    test

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	for (i = 0; i <= 99; i++) {
+		if (i == 99) {
+			Console.WriteLine($"{0:D2}", i);
+				}
+		else {
+            Console.WriteLine($"{0:D2}, ", i);
+        }
+	}
+    }
+}
