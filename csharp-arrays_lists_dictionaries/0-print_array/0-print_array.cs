5678963e3b284330c40616577d4b50109bdabb99
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-02-10 18:40:09 +0100

    fix

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _0_print_array
 {
@@ -6,25 +7,26 @@ namespace _0_print_array
     {
         public static int[] CreatePrint(int size)
         {
-        if (size == 0) {    
-		Console.WriteLine();
-		return new int[size]; 
-			}
-	else if (size < 0) {
+        if (size < 0) {
 		Console.WriteLine("Size cannot be negative");
 		return null;
 			}
-	else {
+
 		int[] arr = new int[size];
+		var output = new StringBuilder();
+
 		for (int i = 0; i < size; i++) {
-			arr[i] = i;
-			if (i < size - 1)
-				Console.Write($"{arr[i]} ");
-			else
-				Console.WriteLine($"{arr[i]}");
+			output.Append(i);
+			if (i != size - 1) {
+
+				output.Append(" ");
 					}
+			arr[i] = i;
+				}
+					}
+		Console.WriteLine(output);
 		return arr;
-		}
-        }
+		
+        
     }
 }
