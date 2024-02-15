f06d35a45c3098c695a65e64161627f528eff312
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-02-15 18:13:30 +0100

    test

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_element_at
+{
+    class Program
+    {
+        public static int elementAt(int[] array, int index)
+        {
+	if (index < 0 || index > array.Length - 1) {
+            Console.WriteLine("Index out of range");
+	    return -1;
+						}
+	return array[index];
+        }
+    }
+}
