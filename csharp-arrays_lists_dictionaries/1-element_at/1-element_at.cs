84c1488fed762cfaade218990217270460d2c14c
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-02-15 19:48:28 +0100

    modify

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,16 +1,16 @@
-﻿using System;
-class Program
+using System;
+class Array
+{
+    public static int elementAt(int[] array, int index)
     {
-        public static int elementAt(int[] array, int index)
+        if (index < 0 || index > array.Length - 1)
         {
-	if (index < 0 || index >= array.Length)
-		{
-		Console.WriteLine("Index out of range");
-		return -1;
-		}
-		else
-		{					
-		return array[index];
-		}
-	}
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+        {
+            return array[index];
+        }
     }
+}
