bfe8a79fef5a20b22dbfd0443c3c5acd7cf9fabe
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-03-18 22:35:45 +0100

    Add files via upload

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_number_keys
+{
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+             int i = 0;
+            foreach(var elemnet in myDict)
+                i++;
+            return i;
+        }
+    }
+}
