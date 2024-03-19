b4a5469b4b4d68b148d1e269f195c93d638084f7
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-03-19 22:22:23 +0100

    Update 9-add_key_value.cs

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,7 +5,12 @@ using System.Collections.Generic;
     {
         public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
         {
-             if (myDict.ContainsKey(key))
+            if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return null;
+        }
+        else if (myDict.ContainsKey(key))
         {
             myDict[key] = value;
         }
