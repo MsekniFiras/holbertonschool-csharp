6e34fda777471a5379b3cad98f90180abc79aed1
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-03-19 22:27:41 +0100

    Add files via upload

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return null;
+        }
+        else if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+        }
+        else
+        {
+            myDict.Add(key, value);
+        }
+        return myDict;
+    }
+}
