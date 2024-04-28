4b4edafa128632b6101df711c7ff7549253fd8f7
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-04-28 18:23:39 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+             List<int> List3 = new List<int>();
+
+        foreach (var elem in list1)
+        {
+            if (list2.Contains(elem))
+            {
+                List3.Add(elem);
+            }
+        }
+        return List3;
+        }
+    }
