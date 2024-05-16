87d4502881b27d7a3128f55d056a83f91c642b2a
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-05-16 18:46:37 +0200

    test

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,23 +5,17 @@ using.System.Collections.Generic;
     {
         public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
         {
-            string topItem;
-        Console.WriteLine("Number of items: {0}", aQueue.Count);
-        if (aQueue.Count == 0)
-        {
-            topItem = "Queue is empty";
-        }
+             Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count  > 0)
+            Console.WriteLine($"First item: {aQueue.Peek()}");
         else
-        {
-            topItem = "First item: " + aQueue.Peek();
-        }
-        Console.WriteLine(topItem);
-        aQueue.Enqueue(newItem);
-        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+            Console.WriteLine("Queue is empty");
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
         while (aQueue.Contains(search))
         {
-            aQueue.Dequeue();
+            aQueue.Dequeue ();
         }
+        aQueue.Enqueue(newItem);
         return aQueue;
         }
     }
