26fb33a3ab096775bc2ee69e447642538b216aa1
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-05-16 19:00:50 +0200

    test

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int Length(LinkedList<int> myLList)
+        {
+             int len = 0;
+        foreach( var element in myList){
+            len++;
+        }
+        return len;
+        }
+    }
