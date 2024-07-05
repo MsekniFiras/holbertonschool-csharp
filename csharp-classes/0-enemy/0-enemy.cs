ff645a194f9a38bf7183fe570cafcf1dcbbd34b9
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-07-05 23:28:00 +0200

    0

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Enemies
+{
+    // This class is empty.
+    /// <summary>
+    /// This class is empty.
+    /// </summary>
+    public class Zombie
+    {
+    
+    }
+}
