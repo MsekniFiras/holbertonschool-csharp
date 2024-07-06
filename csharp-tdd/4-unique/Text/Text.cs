7662c66992639739ab8756db308cd288b45485f2
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-07-06 18:23:23 +0200

    fix

--- csharp-tdd/4-unique/Text/Text.cs
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -2,43 +2,34 @@ using System;
 
 namespace Text
 {
-    /// <summary>
-    /// find first non repeating char
-    /// </summary>
+    /// <summary>Represents a Str class.</summary>
     public class Str
     {
-        /// <summary>
-        /// find first non repeating charachter 
-        /// </summary>
-        /// <param name="s">string to search </param>
-        /// <returns>-1 or index of first non repeating char</returns>
-        public static int UniqueChar(string s)
-        {
+        /// <summary>Checks the index of the first non-repeating character of a string</summary>
+        /// <param name="s">String to check.</param>
+        /// <returns>returns the index of the first non-repeating character of a string.</returns>
+
+        public static int UniqueChar(string s){
             int len;
             int n = 0;
-            int rep = 0;
-            try
-            {
+            int occurs = 0;
+            try{
                 len = s.Length;
             }
-            catch
-            {
+            catch{
                 return -1;
             }
             foreach (var elem in s)
             {
-                for (int i = 0; i < s.Length; i++)
-                {
-                    if (s[i] == elem)
-                    {
-                        rep += 1;
+                for (int i = 0; i < s.Length; i++){
+                    if (s[i] == elem){
+                        occurs += 1;
                     };
                 }
-                if (rep == 1)
-                {
+                if (occurs == 1){
                     return n;
                 }
-                rep = 0;
+                occurs = 0;
                 n += 1;
             }
             return -1;
