0e692aee2a5a888bb12ce507790664d8ef195621
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-07-06 20:16:33 +0200

    animal

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// A simple empty class
+/// </summary>
+class Animal {
+
+}
+/// <summary>
+/// A simple empty, derived class
+/// </summary>
+class Dog:Animal
+{
+}
