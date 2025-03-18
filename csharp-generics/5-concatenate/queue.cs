f8b2b9809521ba067bb8dbc227e8d264004638ee
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2025-03-18 05:13:34 +0100

    test

--- csharp-generics/5-concatenate/queue.cs
+++ csharp-generics/5-concatenate/queue.cs
@@ -85,48 +85,60 @@ class Queue<T>
 
     }
 
-    public T Concatenate()
-    {
-        if (head == null)
-        {
-            Console.WriteLine("Queue is empty");
-            return default(T);
+     /// <summary>
+    /// Concatenates all values in the queue only if the queue is of type String or Char.
+    /// </summary>
+    public String Concatenate(){
+        if (head == null){
+            Console.WriteLine("Queue is empty ");
+            return (null);
         }
-        if (CheckType() == typeof(Char))
-        {
-            string res ="";
-            while (head != null)
-            {
-                res += head.value;
-                head = head.next;
-            }
-            Console.Write(res);
-        }
-
-        if (CheckType() == typeof(String))
-        {
-            string res ="";
-            while (head != null)
-            {
-                res += head.value;
-                if (head.next != null)
-                {
-                    res += " " ;
+        if (typeof(T) == typeof(String) || typeof(T) == typeof(Char)){
+            var concatString = new StringBuilder("");
+            while (head != null){
+                concatString.Append(head.value);
+                if (typeof(T) == typeof(String)){
+                    concatString.Append(" ");
                 }
                 head = head.next;
             }
-            Console.Write(res);
+            return (concatString.ToString());
         }
-        else
-        {
-            Console.WriteLine("Concatenate() is for a queue of Strings or Chars only");
+        Console.WriteLine("Concatenate() is for a queue of Strings or Chars only.");
+        return (null);
+    }
+    /// <summary>
+    /// Gets the count of nodes in the queue.
+    /// </summary>
+    /// <returns>Returns the number of queued elements.</returns>
+    public int Count(){
+        return (this.count);
+    }
+    /// <summary>
+    /// Node of the queue.
+    /// </summary>
+    public class Node{
+        /// <summary>
+        /// Node's value
+        /// </summary>
+        /// <value>Get or set the node's value</value>
+        public T value { get; set; } = default(T);
+
+
+        /// <summary>
+        /// Next node
+        /// </summary>
+        /// <value>Get or set the next node</value>
+        public Node next { get; set; } = null;
+
+
+        /// <summary>
+        /// Default constructor for node class.
+        /// </summary>
+        /// <param name="value">Setting up the node value on initialisation.</param>
+        public Node(T value){
+            this.value = value;
         }
-        return(default(T));
     }
-
-    public int Count()
-    {
-        return(count);
-    }
-
 }
+       
